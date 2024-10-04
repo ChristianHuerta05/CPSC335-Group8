8db4fe24adb8425eb38c5e5bfd69657f38706473
ChristianHuerta05 <Christianhuerta309@gmail.com>
2024-10-03 18:52:11 -0700

    visualization finished

--- app.py
+++ app.py
@@ -9,14 +9,18 @@ def home():
 
 @app.route('/run', methods=['POST'])
 def run_algorithms():
+    # get which algos to run from the post request
     selected_algorithms = {
         "Merge_Sort": 'merge' in request.form,
         "Bubble_Sort": 'bubble' in request.form,
         "Quick_Sort": 'quick' in request.form,
-        "Radix_Sort": 'radix' in request.form
+        "Radix_Sort": 'radix' in request.form,
+        "Linear_Search": 'linear' in request.form,
     }
+    
     input_list = request.form.get('input_list', '')
     times = run(selected_algorithms, input_list)
+    # send back the times and algos ran
     return render_template('index.html', times=times, input_list=input_list)
 
 
--- run.py
+++ run.py
@@ -2,11 +2,12 @@ from bubble_sort import bubble_sort
 from merge_sort import merge_sort
 from quick_sort import quicksort
 from radix_sort import RadixSort
+from linear_search import linear_search
+import tracemalloc
 import time
 
-
 def run(function_options, input_list):
-    data = [3,2,1,4,5,2,1,5,3,2]
+    data = []
     # Cast the input list to a list of integers
     try:
         data = [int(x) for x in input_list.split(', ')]
@@ -14,27 +15,92 @@ def run(function_options, input_list):
         # Handle invalid input
         print("Invalid input")
         pass
-    times = {"Merge_Sort":0,"Bubble_Sort":0,"Quick_Sort":0, "Radix_Sort": 0}
-    print(function_options)
-    if(function_options["Merge_Sort"] == True ):
+    
+    times = {"Merge_Sort": 0, "Bubble_Sort": 0, "Quick_Sort": 0, "Radix_Sort": 0, "Linear_Search": 0}
+
+    if function_options.get("Merge_Sort"):
+        # start tracking time and memory
+        tracemalloc.clear_traces() 
+        tracemalloc.start()
         start = time.perf_counter()
-        merge_sort(data)
-        end = time.perf_counter()
-        times["Merge_Sort"] = end-start
-    if(function_options["Bubble_Sort"] == True):
-        start = time.perf_counter()
-        bubble_sort(data)
-        end = time.perf_counter()
-        times["Bubble_Sort"] = end-start
-    if(function_options["Quick_Sort"] == True):
-        start = time.perf_counter()
-        quicksort(data)
-        end = time.perf_counter()
-        times["Quick_Sort"] = end-start
-    if(function_options["Radix_Sort"] == True):
-        start = time.perf_counter()
-        RadixSort(data)
-        end = time.perf_counter()
-        times["Radix_Sort"] = end-start
+        merge_sort(data.copy()) 
         
+        # stop tracking time 
+        end = time.perf_counter()
+        
+        # from the tracemalloc get back the top memory usage and current usage which is not needed
+        curr, top = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        
+        # Convert bytes to KB for memory usage
+        times["Merge_Sort"] = [end - start, top / 1024]  
+
+    if function_options.get("Bubble_Sort"):
+        # start tracking time and memory
+        tracemalloc.clear_traces() 
+        tracemalloc.start()
+        start = time.perf_counter()
+        bubble_sort(data.copy())
+        
+        # stop tracking time 
+        end = time.perf_counter()
+        
+        # from the tracemalloc get back the top memory usage and current usage which is not needed
+        curr, top = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        
+        times["Bubble_Sort"] = [end - start, top / 1024]
+
+    if function_options.get("Quick_Sort"):
+        # start tracking time and memory
+        tracemalloc.clear_traces() 
+        tracemalloc.start()
+        start = time.perf_counter()
+        
+        try:
+            quicksort(data.copy())
+        except RecursionError:
+            # stop tracking due to recursion being too deep on large amounts of data
+            end = time.perf_counter()
+            curr, top = tracemalloc.get_traced_memory()
+            tracemalloc.stop()
+            times["Quick_Sort"] = ["RecursionError", top / 1024]
+            return times  
+        
+        # stop tracking time 
+        end = time.perf_counter()
+        curr, top = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        
+        times["Quick_Sort"] = [end - start, top / 1024]
+
+    if function_options.get("Radix_Sort"):
+        # start tracking time and memory
+        tracemalloc.clear_traces() 
+        tracemalloc.start()
+        start = time.perf_counter()
+        
+        RadixSort(data.copy())
+        
+        # stop tracking time and memory
+        end = time.perf_counter()
+        curr, top = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        
+        times["Radix_Sort"] = [end - start, top / 1024]
+        
+    if function_options.get("Linear_Search"):
+        # start tracking time and memory
+        tracemalloc.clear_traces() 
+        tracemalloc.start()
+        start = time.perf_counter()
+        linear_search(data.copy(), 101)
+        
+        # stop tracking time and memory
+        end = time.perf_counter()
+        curr, top = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        
+        times["Linear_Search"] = [end - start, top / 1024]
+
     return times
